2b613ce003e933918d25f4f413647adf87caeba0
Derek Finlinson <derek.finlinson@journeyteam.com>
2017-06-01 10:26:16 -0600

    Finish upload method

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var webApi = require('CRMWebAPI');
 var adal = require('adal-node');
 var path = require('path');
+var fs = require('fs');
 
 var WebResource = function() {};
 
@@ -32,6 +33,8 @@ function getWebResourceType(type) {
 }
 
 WebResource.prototype.upload = function (config, assets) {
+    console.log("\r\nUploading web resources...");
+
     return new Promise(function (resolve, reject) {
         // Authenticate
         var authorityHostUrl = 'https://login.windows.net/' + config.tenant;
@@ -43,90 +46,95 @@ WebResource.prototype.upload = function (config, assets) {
                 return;
             }
 
-            var apiconfig = {
-                APIUrl: config.server + '/api/data/v8.0/',
+            var apiConfig = {
+                APIUrl: config.server + `/api/data/v8.0/`,
                 AccessToken: token.accessToken
             };
 
-            var api = new webApi(apiconfig);
+            var api = new webApi(apiConfig);
 
-            var promises = assets.map((asset) => {
+            // Retrieve assets from CRM
+            const resources = Promise.all(assets.map((asset) => {
                 // Get web resource from config
                 var resource = config.webResources.filter((wr) => {
-                    return wr.path === asset.path;
+                    return wr.path === asset;
                 });
                 
                 if (resource.length === 0) {
-                    reject("Web resource " + file.path + " is not configured");
-                    return;
-                }
+                    console.log("Web resource " + asset + " is not configured");
+                    return Promise.resolve();
+                } else {
+                    resource = resource[0];
 
-                resource = resource[0];
-
-                // Check if web resource already exists
-                var option = {
-                    Filter: "name eq '" + resource.uniqueName + "'",
-                    Select: [ 'webresourceid' ]
-                };
-
-                return api.GetList("webresourceset", option);
-            });
-
-            Promise.all(promises).then((results) => {
-                results.reduce((result) => {
-
-                });
-                
-                var updatePromises = results.map((result) => {
-                    // Create or update web resource
-                    var webResource = {
-                        content: file.source
+                    // Check if web resource already exists
+                    var option = {
+                        Filter: "name eq '" + resource.uniqueName + "'",
+                        Select: [ 'webresourceid' ]
                     };
 
-                    if (result.List.length === 0) {
-                        webResource.webresourcetype = getWebResourceType(resource.type);
-                        webResource.name = resource.uniqueName;
-                        webResource.displayname = resource.displayName || resource.uniqueName
+                    return api.GetList("webresourceset", option).then((result) => {
+                        // Create or update web resource
+                        var webResource = {
+                            content: fs.readFileSync(asset).toString()
+                        };
 
-                        return api.Create("webresourceset", webResource);
-                    } else {
-                        return api.Update("webresourceset", result.List[0].webresourceid, webResource);
+                        if (result.List.length === 0) {
+                            webResource.webresourcetype = getWebResourceType(resource.type);
+                            webResource.name = resource.uniqueName;
+                            webResource.displayname = resource.displayName || resource.uniqueName
+
+                            return api.Create("webresourceset", webResource);
+                        } else {
+                            return api.Update("webresourceset", result.List[0].webresourceid, webResource);
+                        }
+                    });
+                }            
+            })).then((upserts) => {
+                console.log("Publishing web resources...");
+
+                // Get updates
+                var updates = upserts.filter((u) => {
+                    return u.hasOwnProperty("EntityID");
+                }).map((u) => `<webresource>{${u.EntityID}}</webresource>`).join("");
+                
+                // Get inserts
+                var inserts = upserts.filter((u) => {
+                    return !u.hasOwnProperty("EntityID");
+                });
+
+                var tasks = [];
+
+                // Publish all updates at once
+                tasks.push({
+                    action: "PublishXml",
+                    data: {
+                        ParameterXml: `<importexportxml><webresources>${updates}</webresources></importexportxml>`
                     }
                 });
 
-                Promise.all(updatePromises).then((upserts) => {
-                    for (var upsert of upserts) {
-                        if (upsert.hasOwnProperty("id")) {
-                            // Add to solution
-                            var params = {
-                                ComponentId: id,
-                                ComponentType: 61,
-                                SolutionUniqueName: config.solution,
-                                AddRequiredComponents: false,
-                                IncludedComponentSettingsValues: null
-                            };
-
-                            api.ExecuteAction("AddSolutionComponent", params).then(function () {
-                                resolve(file.path + " added to solution " + config.solution);
-                            }, function (error) {
-                                reject(error);
-                            });
-
-                            // Publish
-                                var publish = {
-                                    ParameterXml: "<importexportxml><webresources><webresource>{" + 
-                                    result.List[0].webresourceid + "}</webresource></webresources></importexportxml>"
-                                };
-                                
-                                api.ExecuteAction("PublishXml", publish).then(function () {
-                                    resolve(file.path + " published");
-                                }, function (error) {
-                                    reject(error);
-                                }, function (error) {
-                                    reject(error);
-                                });
+                // Add solution component individually
+                inserts.forEach((i) => {
+                    var item = {
+                        action: "AddSolutionComponent",
+                        data: {
+                            ComponentId: upsert,
+                            ComponentType: 61,
+                            SolutionUniqueName: config.solution,
+                            AddRequiredComponents: false,
+                            IncludedComponentSettingsValues: null
                         }
-                    }
+                    };
+
+                    tasks.push(item);
+                });
+
+                tasks.reduce((current, task) => {
+                    return current.then(() => {
+                        return api.ExecuteAction(task.action, task.data);                        
+                    });
+                }, Promise.resolve()).then(() => {
+                    console.log("Uploaded and published web resources\r\n");
+                    resolve();
                 }, (error) => {
                     reject(error);
                 });
